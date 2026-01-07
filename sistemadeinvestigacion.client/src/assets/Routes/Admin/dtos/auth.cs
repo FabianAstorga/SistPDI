6fadf72527a20bd0f20946a99a0d8c50d2bc5c5d
Angel Caiman <Angel_6439@hotmail.com>
2026-01-07 17:20:17 -0300

    controlador auth y dtos

---
+++ sistemadeinvestigacion.client/src/assets/Routes/Admin/dtos/auth.cs
@@ -0,0 +1,8 @@
+namespace TuProyecto.Shared.Models // Usa tu namespace real
+{
+    public class LoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
