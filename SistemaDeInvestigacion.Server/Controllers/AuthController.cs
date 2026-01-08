ef00ee8bfdd470e7b8eb8064c258904390c9146d
FabianAstorga <74470899+FabianAstorga@users.noreply.github.com>
2026-01-08 09:07:44 -0300

    Bv

--- SistemaDeInvestigacion.Server/Controllers/AuthController.cs
+++ SistemaDeInvestigacion.Server/Controllers/AuthController.cs
@@ -1,7 +1,7 @@
-﻿using BCrypt.Net;                    // Si usaste la librería de encriptación que hablamos
+﻿using BCrypt.Net;                    
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore; // IMPORTANTE: Necesario para usar bases de datos
-using SistemaDeInvestigacion.Server.Data;        // Donde tengas tu DbContext
+using Microsoft.EntityFrameworkCore; 
+using SistemaDeInvestigacion.Server.Data;       
 
 namespace TuProyecto.Server.Controllers
 {
@@ -9,10 +9,8 @@ namespace TuProyecto.Server.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        // 1. VARIABLE PARA LA BASE DE DATOS
         private readonly ApplicationDbContext _context;
 
-        // 2. CONSTRUCTOR: Aquí "inyectamos" la conexión a la BD
         public AuthController(ApplicationDbContext context)
         {
             _context = context;
@@ -21,36 +19,33 @@ namespace TuProyecto.Server.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginDto loginRequest)
         {
-            // PASO A: BUSCAR EL USUARIO POR CORREO
-            // "Busca en la tabla Usuarios el primero que tenga este Email"
+            
             var usuarioEncontrado = await _context.Usuarios
                                           .FirstOrDefaultAsync(u => u.Email == loginRequest.Email);
 
-            // PASO B: VALIDAR SI EXISTE
+            
             if (usuarioEncontrado == null)
             {
                 return Unauthorized(new { Message = "Correo o contraseña incorrectos" });
             }
 
-            // PASO C: VERIFICAR LA CONTRASEÑA
-            // OPCIÓN 1: Si usaste BCrypt (Recomendado/Profesional)
+            
             bool passValida = BCrypt.Net.BCrypt.Verify(loginRequest.Password, usuarioEncontrado.Password);
 
-            // OPCIÓN 2: Si NO estás encriptando (Solo texto plano - NO recomendado pero funcional)
-            // bool passValida = (loginRequest.Password == usuarioEncontrado.Password);
+            
 
             if (!passValida)
             {
                 return Unauthorized(new { Message = "Correo o contraseña incorrectos" });
             }
 
-            // PASO D: RETORNAR ÉXITO
+            
             return Ok(new
             {
-                Token = "token_real_generado_aqui", // Aquí luego implementaremos JWT
+                Token = "token_real_generado_aqui", 
                 User = new
                 {
-                    Name = usuarioEncontrado.Nombre, // O el campo que tengas
+                    Name = usuarioEncontrado.Nombre, 
                     Email = usuarioEncontrado.Email
                 }
             });
